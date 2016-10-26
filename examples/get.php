ec49db7bd5ea52f7f0df88c672ccf419a6aaacdb
pengshuai25 <pengshuai@shopex.cn>
2016-10-26 15:27:39 +0800

    Update get.php

--- examples/get.php
+++ examples/get.php
@@ -13,11 +13,8 @@ require_once(__DIR__ . '/../src/TeegonClient.php');
 // 第四个参数 $socket socket文件地址，如果有则优先选择socke方式 ,$socket = "unix:///tmp/api_provider.sock"
 $client = new TeegonClient($url = 'http://api.teegon.com/router', $key = 'xjMdeBd4h', $secret = 'FkJdftb5wgeE4dSNYX8waj4');
 
-$param = [
-    'user_eid' => 'test',
-    'app_id' => '123456',
-];
+
 // 发起请求
-$res = $client->get('shopex.query.appqueue' ,$param);
+$res = $client->post('shopex.queue.read', array('topic'=>'orders', 'num'=>1,'drop'=>false));
 echo $res;
 // 返回: pong
